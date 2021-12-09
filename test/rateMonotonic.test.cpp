28e8e63473d0eb57ebba58154b61e6282f975f01
Tiago <tmsilva11@usp.br>
2021-12-09 19:13:13 -0300

    Add test for A*

--- test/rateMonotonic.test.cpp
+++ test/rateMonotonic.test.cpp
@@ -7,13 +7,12 @@
 using namespace RTSSCheduler;
 using namespace RTSTasks;
 
-TEST_CASE("RateMonotonicScheduler test"){
-	int expected[13] {0, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3}; 
+TEST_CASE("RateMonotonicScheduler test") {
 	
 	auto taskA = TaskBuilder::createPeriodicTask(0, 4, 1, 1);
 	auto taskB = TaskBuilder::createPeriodicTask(0, 6, 3, 6);
 	
-	SUBCASE("Preload feasible set of tasks doesn't throw exception"){
+	SUBCASE("Preload feasible set of tasks doesn't throw exception") {
 		RateMonotonicScheduler scheduler;
 		scheduler.preloadTask(taskA);
 		scheduler.preloadTask(taskB);
@@ -21,7 +20,7 @@ TEST_CASE("RateMonotonicScheduler test"){
 		CHECK_NOTHROW(scheduler.prepareScheduler());
 	}
 
-	SUBCASE("Preload infeasible set of tasks throws exception"){
+	SUBCASE("Preload infeasible set of tasks throws exception") {
 		RateMonotonicScheduler scheduler;
 		scheduler.preloadTask(taskA);
 		scheduler.preloadTask(taskB);
@@ -30,5 +29,21 @@ TEST_CASE("RateMonotonicScheduler test"){
 		scheduler.preloadTask(taskA);
 
 		CHECK_THROWS_AS(scheduler.prepareScheduler(), const std::runtime_error&);
+	}
+
+	SUBCASE("A* calculation") {
+		unsigned expected[13] {0, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3}; 
+		
+		RateMonotonicScheduler scheduler;
+		scheduler.preloadTask(taskA);
+		scheduler.preloadTask(taskB);
+		scheduler.prepareScheduler();
+		
+		REQUIRE(scheduler.ap_proc_time_zero_H.size() == sizeof(expected) / sizeof(unsigned));
+		
+		for(int i = 0; i < scheduler.ap_proc_time_zero_H.size(); i++)
+		{
+			CHECK(scheduler.ap_proc_time_zero_H[i] == expected[i]);
+		}
 	}
 }
