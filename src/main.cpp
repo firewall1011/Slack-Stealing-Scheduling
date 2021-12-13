8055ed82cc1dfc074c0161c98a82278ee79898fe
Van Loon <gabriel.vl.rojas@gmail.com>
2021-12-13 10:51:03 -0300

    update main to dinamic create scheduler and tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,115 @@
 #include <iostream>
 #include <unistd.h>
+#include <string>
+#include "utils/outputs.hpp"
 #include "tasks/task.hpp"
 #include "tasks/taskbuilder.hpp"
 #include "schedulers/rateMonotonic.hpp"
+#include "schedulers/slackStealing.hpp"
 
 using namespace RTSTasks;
 using namespace RTSSCheduler;
 
 int main(void){
 
-    try 
-    { 
-        RateMonotonicScheduler scheduler;
+    // User Inputs
+    int userOpt = -1, taskArrival, taskCost, taskPeriod, taskDeadline;
+    std::string taskName;
 
-        scheduler.preloadTask(TaskBuilder::createPeriodicTask(0, 3, 1, 1));
-        scheduler.preloadTask(TaskBuilder::createPeriodicTask(0, 6, 1, 3));
-        scheduler.preloadTask(TaskBuilder::createTask(3, 4));
+    // Variables
+    Task auxTask = Task(0,0,0,0,0,0,0);
+    BaseScheduler* scheduler = nullptr;
+    
+    // Program Pré- Configurations
+    printHeader();
 
-        scheduler.prepareScheduler();
+    // 1 ~ Selecting the Scheduler
+    printf("[!] Select the Scheduler: [0] Rate Monotonic | [1] Slack Stealing: ");
+    std::cin >> userOpt;
 
-        scheduler.start();
+    if(userOpt == 0)
+        scheduler = new RateMonotonicScheduler();
+    else if (userOpt == 1)
+        scheduler = new SlackStealingScheduler();    
+    else
+    {
+        printf("[x] Error: Invalid Scheduler Selection! Exited!");
+        return EXIT_FAILURE;
+    }
 
-        std::cout << "Starting scheduler..." << std::endl;
+    // 2 ~ Preloading Tasks
+    userOpt = -1;
+    while(userOpt != 0)
+    {
+        printf("[ ]\n");
+        printf("[!] Create New Task: [0] Done | [1] Periodic | [2] Aperiodic: ");
+        std::cin >> userOpt;
 
-        while(true)
-        {
-            scheduler.tick();
-            std::cin.get();
+        if(userOpt == 0)
+            break;
+        
+        else if(userOpt == 1)
+        {   
+            try
+            {   
+                printf("[!] <Name, Cost, Period, Deadline>: "); 
+                std::cin >> taskName >> taskCost >> taskPeriod >> taskDeadline;
+                auxTask = TaskBuilder::createPeriodicTask(0, taskCost, taskPeriod, taskDeadline);
+                auxTask.name = taskName;
+                scheduler->preloadTask(auxTask);
+                std::cout << "[+] Task " << taskName << " preloaded!" << std::endl;
+            }
+            catch(const std::exception& e)
+            {
+                std::cout << "[x] Error in " << e.what() << std::endl;
+            }
         }
 
-    } 
-    catch(const std::exception& ex) 
-    { 
-        std::cerr << ex.what() << std::endl;
+        else if(userOpt == 2)
+        {
+            try
+            {   
+                printf("[!] <Name, Cost, ArrivalTime>: ");
+                std::cin >> taskName >> taskCost >> taskArrival;
+                auxTask = TaskBuilder::createTask(taskArrival, taskCost);
+                auxTask.name = taskName;
+                scheduler->preloadTask(auxTask);
+                std::cout << "[+] Task " << taskName << " preloaded!" << std::endl;
+            }
+            catch(const std::exception& e)
+            {
+                std::cout << "[x] Error in " << e.what() << std::endl;
+            }
+        }
+
+        else
+        {
+            printf("[x] Error: Invalid Task Option! Exited!");
+            return EXIT_FAILURE;
+        }
+    }
+    
+    // 3 ~ Running the scheduler
+    try
+    {   
+        printf("[ ]\n");
+        printf("[+] Preparing the scheduler...\n");
+        scheduler->prepareScheduler();
+
+        printf("[+] Starting the scheduler...\n");
+        scheduler->start();
+
+        printf("[+] Scheduler started: [ENTER] to tick() | [EOF] to quit!\n");
+        printf("[ ]\n");
+
+        while(std::cin.get() != EOF)
+        {
+            scheduler->tick();
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "[x] Error in " << e.what() << std::endl;
     }
 
     return EXIT_SUCCESS;
